03222b90f614d312b67902d99fdfc608a18be01b
evicente558432-max <e.vicente.558432@umindanao.edu.ph>
2026-03-16 19:32:19 +0800

    Delete Miniport directory

--- Miniport/script.js
+++
@@ -1,5 +0,0 @@
-
-function openModal(projectId) {
-  window.location.href = 'project-detail.php?id=' + projectId;
-document.getElementById('modalDetailsLink').href = 'project-detail.php?id=' + projectId;
-}
